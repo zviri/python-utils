ab1175cb53c5df5b5164231392a713dfb998817e
Peter Zvirinsky <peter.zvirinsky@firma.seznam.cz>
2015-03-16 18:18:16 +0100

    initial commit

---
+++ stringutils.py
@@ -0,0 +1,10 @@
+import unicodedata
+import re
+
+def remove_accents(s):
+    nkfd_form = unicodedata.normalize('NFKD', s)
+    only_ascii = nkfd_form.encode('ASCII', 'ignore')
+    return only_ascii
+
+def normalize_whitespace(s):
+    return re.sub(r'\s+', ' ', s.strip())
